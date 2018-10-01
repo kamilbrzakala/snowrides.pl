c9770a8ee5c27f44e91935105b64a6f17c60ccab
bzykson <bzykson@gmail.com>
2018-10-01 23:27:22 +0200

    taksidi.py commit

--- HTMLparsers/taksidi.py
+++ HTMLparsers/taksidi.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 from bs4.element import Comment
-import re, datetime, json, requests, os, time
+import re, datetime, json, requests, os, time,csv
 from collections import OrderedDict
 
 
@@ -22,121 +22,107 @@ freeslot = []
 country = []
 where = []
 price = []
+pricepromo = []
 date = []
-tempDate = []
 
 print("TAKSIDI in progress...")
-for div in soup.find_all('div', attrs={'class':'oneproduct'}):
-    print("in loop")
+for div in soup.find_all('a', attrs={'class':'trip-single'}):
+
     i += 1
-    # for i, n in enumerate(div):
-    #     print(i,": ",n)
 
-    # # #
-
-    div_place_where = div.find('h2').get_text()
-    print(div)
-
-    if "@" in div_place_where:
-        city = div_place_where.split("@")[1].strip()
-        # print(city)
-
-    div_where = re.search('@ (.*)|- (.*)', div_place_where)
-    #print(div_where)
-    # d1 = div_where.group(1)
-    # d2 = div_where.group(2)
-
-    try:
-        # where.append(div_place_where.strip().split(" - ")[0].split("@")[1])
-        where.append(city)
-    except IndexError:
-        where.append(city)
-        # where.append(div_place_where.strip().split(" - ")[0])
-
-    # # # #
-    div_country = div.find('div', 'placeProduct').get_text().strip().split(' ')
+    div_country = div.find('div','trip-place')
+    div_country = div_country.find('p').get_text().strip()
+    country.append(div_country)
     # print(div_country)
-    try:
-        country.append(div_country[1])
-    except IndexError:
-        country.append(div_country[0])
 
-    # # # #
-    div_date = (div.find('div', attrs={'class': 'dateProduct'}).get_text().strip())
+    div_place_where = div.find('div','trip-desc')
+    # print(div_place_where)
+    div_place_where = div_place_where.find('h3').get_text().strip()
+    # prints the p tag content
+    if("@" in div_place_where):
+        div_place_where.split(" @ ")[1].strip()
+        # print(div_place_where.split(" @ ")[1].strip())
+        where.append(div_place_where.split(" @ ")[1].strip())
+    else:
+        where.append(div_place_where)
+        # print(div_place_where)
+
+    div_date = div.find('div','trip-date')
+    div_date = div_date.find('p').get_text().strip()
+    # print(div_date)
     date.append(div_date)
-    div_place_date = div_date.split(' - ')
-    # print("div_place_date", div_place_date)
-    # # # #
-    dt = [div.strip() for div in div_place_date][0]
-    tempDate.append(dt)
-    # print("DT", dt)
-    # # # #
-    div_price = (div.find('div', attrs={'class': 'priceProductPrice'}).get_text())
-    price.append(div_price.strip())
-    # # # #
-    link = ("https://taksidi.pl" + div.find('a')['href'])
+
+    div_price_original = div.find('div','trip-price')
+
+    div_price_original = div_price_original.find('p','trip-price-old')
+    div_price_promo = div.find('p','trip-price-promo')
+    # div_price_promo = div_price_promo.find('p').get_text().strip()
+
+    if(div_price_original is None):
+        price.append("none")
+        # print("none")
+    else:
+        # print(div_price_original.get_text().strip())
+        price.append(div_price_original.get_text().strip())
+
+    if(div_price_promo is None):
+        pricepromo.append("none")
+        # print("none")
+    else:
+        # print(div_price_promo.get_text().strip())
+        pricepromo.append(div_price_promo.get_text().strip())
+
+    div_slot = div.find("div", "trip-price-down")
+    # print(div_slot)
+    # gets 2nd 'p' sibling from parent tag:
+    div_slot = div_slot.select("p:nth-of-type(2)")
+    if(div_slot):
+        # print(div_slot[0].get_text().strip())
+        freeslot.append(div_slot[0].get_text().strip())
+    else:
+        freeslot.append("brak danych")
+        # print("brak danych")
+
+    link = div['href']
+    link = "https://taksidi.pl" + link
+    # print(link)
     links.append(link)
-    # # # #
 
-for pg in links:
-    try:
-        response = requests.get(pg, headers=headers)
-        res_status = response.status_code
-
-        soup3 = BeautifulSoup(response.content, "html.parser")
-
-        if res_status == 200:
-            file = open(r'C:\Users\PREZES\Desktop\snowridess\venv\html\taksidifreeslots.html', 'a', encoding="utf8")
-            file.write(str(soup3.prettify()))
-            file.close()
-
-
-    except IndexError as exc:
-        print('There was a problem with error: {}'.format(exc))
-
-free_slot_file = open(r'C:\Users\PREZES\Desktop\snowridess\venv\html\taksidifreeslots.html', encoding="utf8")
-soup2 = BeautifulSoup(free_slot_file, "html.parser")
-free_slot_file.close()
-# poniżej szukam informacji na temat wolnych miejsc
-for div in soup2.find_all('table', attrs={'style':'width:100%; margin:10px 0; font-size:18px;'}):
-    try:
-        freeslot.append(div.get_text("|", strip=True).split("|")[13])
-        # print(div.get_text("|", strip=True).split("|")[13])
-    except IndexError:
-        freeslot.append("Brak danych")
-        # print("Brak danych")
-
-for i, slott in enumerate(where):
+for i, value in enumerate(where):
     store_info = {}
 
-    if where[i] not in "Chorwacja":
-        # print("DATE:", date[i].strip(" - ")[0], now)
-        if tempDate[i] >= now and tempDate[i] > "2018-12-01":
-            store_info = {
-                            'Organizator': "TAKSIDI",
-                            'City': where[i],
-                            'Country': country[i],
-                            'Date': date[i],
-                            'Price': price[i],
-                            'Free slots': slott,
-                            'Link': links[i]
+            # if tempDate[i] >= now and tempDate[i] > "2018-12-01":
+    store_info = {
+                                'Organizator': "TAKSIDI",
+                                'City': where[i],
+                                'Country': country[i],
+                                'Date': date[i],
+                                'Price': price[i],
+                                'Free slots': freeslot[i],
+                                'Link': links[i]
 
-                        }
+                            }
 
-            print("{0}:\n Gdzie: {1} \n Kraj: {2} \n Kiedy: {3}\n Cena: {4}\n Link: {5}\n Wolne miejsca: {6}".
-                          format(i, where[i], country[i], date[i], price[i], slott, links[i], '\n'))
+    print("{0}:\n Gdzie: {1} \n Kraj: {2} \n Kiedy: {3}\n Cena: {4}\n Wolne miejsca: {5}\n Link: {6}".
+                              format(i, where[i], country[i], date[i], price[i], freeslot[i], links[i], '\n'))
 
-            scraped_store_taksidi.append(store_info)
+    scraped_store_taksidi.append(store_info)
 
-# poniżej otwieram plik json
-# with open('taksidi.json', 'w') as json_file:
-#     json.dump(scraped_store_taksidi, json_file, indent=4, ensure_ascii=False)
-time.sleep(0.2)
-# usuwam tymczasowy plik potrzebny do pobrania listy wolnych miejsc
-try:
-    os.remove(r'C:/Users/PREZES/Desktop/snowridess/venv/html/taksidifreeslots.html')
-    print("Plik ""taksidifreeslots.html"" został usunięty pomyślnie.")
-    print("TAKSIDI done")
-except OSError:
-    print(OSError.strerror )
-    pass
+# print(scraped_store_taksidi[1])
+with open(r'C:\Users\PREZES\Desktop\snowridess\venv\taksidi.csv', 'w', newline='', encoding="utf8") as csvfile:
+
+    for i in range(len(scraped_store_taksidi)):
+        # print(scraped_store_taksidi[i]["Country"])
+        fieldnames = ['country', 'city', 'Date', 'Price', 'Free slots', 'Link']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerow({
+                        "country": scraped_store_taksidi[i]["Country"],
+                         "city": scraped_store_taksidi[i]["City"],
+                         "Date": scraped_store_taksidi[i]["Date"],
+                         "Price": scraped_store_taksidi[i]["Price"],
+                         "Free slots": scraped_store_taksidi[i]["Free slots"],
+                         "Link": scraped_store_taksidi[i]["Link"]
+                         })
+
+# print("temp.csv created successfully")
